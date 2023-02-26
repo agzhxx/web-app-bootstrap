40b73ea8eb3b5bcb116a1128cdf53743fbf0bb4e
agzhxx <118198588+agzhxx@users.noreply.github.com>
2023-02-27 02:58:08 +0800

    Add files via upload

---
+++ W2Agency/Js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    $('#item1').on('click', function () {
+        $('#item0').text('EN');
+    });
+
+    $('#item2').on('click', function () {
+        $('#item0').text('RU');
+    });
+    $('#item3').on('click', function () {
+        $('#item0').text('KZ');
+    });
+});
+
+$(document).ready(function () {
+
+    $('#ditem1').on('click', function () {
+        $('#ditem0').text('EN');
+    });
+
+    $('#ditem2').on('click', function () {
+        $('#ditem0').text('RU');
+    });
+    $('#ditem3').on('click', function () {
+        $('#ditem0').text('KZ');
+    });
+});
